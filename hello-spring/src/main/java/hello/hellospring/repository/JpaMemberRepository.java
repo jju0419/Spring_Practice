de82cc70f17b7ee913962208efb09774fe23e65f
jju0419 <56835376+jju0419@users.noreply.github.com>
2023-12-07 20:25:21 +0900

    .

--- hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -12,9 +12,7 @@ public class JpaMemberRepository implements MemberRepository{
 
     private final EntityManager em;
 
-    public JpaMemberRepository(EntityManager em) {
-        this.em = em;
-    }
+    public JpaMemberRepository(EntityManager em) {this.em = em;}
 
 
     @Override
